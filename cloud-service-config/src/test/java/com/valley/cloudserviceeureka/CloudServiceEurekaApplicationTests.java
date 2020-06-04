24a99777f996b6a5a3b88fbb0789ef045d9c4088
zhuhongmin <zhm1230982006@qq.com>
2020-06-04 13:58:32 +0800

    初始化

---
+++ cloud-service-config/src/test/java/com/valley/cloudserviceeureka/CloudServiceEurekaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.valley.cloudserviceeureka;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServiceEurekaApplicationTests {
+
+
+    void contextLoads() {
+    }
+
+}
