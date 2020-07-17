87a1c06b7e515c2b07ebf39d8c23ab032f94b205
zhuhongmin <zhm1230982006@qq.com>
2020-07-17 13:37:12 +0800

    修改配置

--- cloud-service-auth/src/main/java/com/valley/cloudservice/auth/config/RedisConfig.java
+++ cloud-service-auth/src/main/java/com/valley/cloudservice/auth/config/RedisConfig.java
@@ -1,12 +1,38 @@
 package com.valley.cloudservice.auth.config;
 
+
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.redis.JedisClientConfigurationBuilderCustomizer;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StringUtils;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.net.UnknownHostException;
+import java.time.Duration;
 
 @Configuration
-public class RedisConfig {
+//@EnableConfigurationProperties(RedisProperties.class)
+public class RedisConfig extends RedisConnection  {
+    private final RedisProperties properties;
+
+    private final ObjectProvider<JedisClientConfigurationBuilderCustomizer> builderCustomizers;
+
+    public RedisConfig(RedisProperties properties, ObjectProvider<RedisSentinelConfiguration> sentinelConfigurationProvider, ObjectProvider<RedisClusterConfiguration> clusterConfigurationProvider, RedisProperties properties1, ObjectProvider<JedisClientConfigurationBuilderCustomizer> builderCustomizers) {
+        super(properties, sentinelConfigurationProvider, clusterConfigurationProvider);
+        this.properties = properties1;
+        this.builderCustomizers = builderCustomizers;
+    }
 
     @Bean
     public RedisTemplate<String,String> redisTemplate(RedisConnectionFactory redisConnectionFactory){
@@ -14,5 +40,79 @@ public class RedisConfig {
         RedisTemplate<String,String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(RedisConnectionFactory.class)
+    public JedisConnectionFactory redisConnectionFactory() throws UnknownHostException {
+        return createJedisConnectionFactory();
+    }
+
+    private JedisConnectionFactory createJedisConnectionFactory() {
+        JedisClientConfiguration clientConfiguration = getJedisClientConfiguration();
+        if (getSentinelConfig() != null) {
+            return new JedisConnectionFactory(getSentinelConfig(), clientConfiguration);
+        }
+        if (getClusterConfiguration() != null) {
+            return new JedisConnectionFactory(getClusterConfiguration(),
+                    clientConfiguration);
+        }
+        return new JedisConnectionFactory(getStandaloneConfig(), clientConfiguration);
+    }
+
+    private JedisClientConfiguration getJedisClientConfiguration() {
+        JedisClientConfiguration.JedisClientConfigurationBuilder builder = applyProperties(
+                JedisClientConfiguration.builder());
+        RedisProperties.Pool pool = this.properties.getJedis().getPool();
+        if (pool != null) {
+            applyPooling(pool, builder);
+        }
+        if (StringUtils.hasText(this.properties.getUrl())) {
+            customizeConfigurationFromUrl(builder);
+        }
+        customize(builder);
+        return builder.build();
+    }
+
+    private JedisClientConfiguration.JedisClientConfigurationBuilder applyProperties(
+            JedisClientConfiguration.JedisClientConfigurationBuilder builder) {
+        if (this.properties.isSsl()) {
+            builder.useSsl();
+        }
+        if (this.properties.getTimeout() != null) {
+            Duration timeout = this.properties.getTimeout();
+            builder.readTimeout(timeout).connectTimeout(timeout);
+        }
+        return builder;
+    }
+
+    private void applyPooling(RedisProperties.Pool pool,
+                              JedisClientConfiguration.JedisClientConfigurationBuilder builder) {
+        builder.usePooling().poolConfig(jedisPoolConfig(pool));
+    }
+
+    private JedisPoolConfig jedisPoolConfig(RedisProperties.Pool pool) {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(pool.getMaxActive());
+        config.setMaxIdle(pool.getMaxIdle());
+        config.setMinIdle(pool.getMinIdle());
+        if (pool.getMaxWait() != null) {
+            config.setMaxWaitMillis(pool.getMaxWait().toMillis());
+        }
+        return config;
+    }
+
+    private void customizeConfigurationFromUrl(
+            JedisClientConfiguration.JedisClientConfigurationBuilder builder) {
+        RedisConnection.ConnectionInfo connectionInfo = parseUrl(this.properties.getUrl());
+        if (connectionInfo.isUseSsl()) {
+            builder.useSsl();
+        }
+    }
+
+    private void customize(
+            JedisClientConfiguration.JedisClientConfigurationBuilder builder) {
+        this.builderCustomizers.orderedStream()
+                .forEach((customizer) -> customizer.customize(builder));
     }
 }
